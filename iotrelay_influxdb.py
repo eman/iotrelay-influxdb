85dab031e14eebdec2b520c03444abbf206916f3
Emmanuel Levijarvi <emansl@gmail.com>
2016-12-27 00:20:17 -0800

    Bump version: 1.7.0 → 1.7.1

--- iotrelay_influxdb.py
+++ iotrelay_influxdb.py
@@ -10,7 +10,7 @@ from influxdb.exceptions import InfluxDBServerError, InfluxDBClientError
 
 logger = logging.getLogger(__name__)
 
-__version__ = "1.7.0"
+__version__ = "1.7.1"
 
 DEFAULT_BATCH_SIZE = 30
 HOST = 'localhost'
